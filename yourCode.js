ce684cab0f19305dc9fd10a472f05e9180346a60
Felipe Carmo <carmof@hanover.edu>
2015-01-18 15:52:50 -0500

    Activity 1 done. Close #2

--- yourCode.js
+++ yourCode.js
@@ -10,13 +10,28 @@ var binarySearch = function binarySearch(arr, val) {
    var lo, hi, mid;
 
    // You may need to add things here
-
-   while (false) {     // You should change this with a proper condition
-      // You will need to add things here
+   lo = 0;
+   hi = arr.length-1;
+   mid = Math.floor(hi/2);
+   if(hi < 0 || hi === undefined){
+   	  return false;
    }
-
+   while ((lo <= hi)) {     // You should change this with a proper condition
+   	  mid = Math.floor((hi+lo)/2);
+   	  if(mid === hi || mid ===lo){
+   	  	return (arr[lo] === val || arr[hi] === val);
+   	  }
+   	  if(arr[mid] === val){
+   		return true;
+   	  }
+      if(arr[mid] < val){
+      	lo = mid;
+      }else{
+      	hi = mid;
+      }
+   }
    // You may need to add things here
-
+   return false;
 };
 
 /*
@@ -24,10 +39,12 @@ var binarySearch = function binarySearch(arr, val) {
  */
 var countTags = function countTags(items) {
    // Declare your local variables here. One was done for you.
-   var tagCounts;
+   var tagCounts = 0;
 
    // Add your code here
+   items.forEach(function(item, index){
 
+   });
 
    return tagCounts;
 };
