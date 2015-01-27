f4c376f8b27c3985e42ae3a3519c3afc02fa7df0
carmof <carmof@hanover.edu>
2015-01-26 21:38:09 -0500

    Classe mapCode finished

--- map/mapCode.js
+++ map/mapCode.js
@@ -1,6 +1,6 @@
 /*
- * Name 1: YourNameHere
- * Name 2: YourNameHere
+ * Name 1: Felipe Carmo
+ * Name 2: Filipe Oliveira
  */
 
 // Do not change the name of this function
@@ -12,11 +12,70 @@ var makeMap = function() {
    var storedPairs;
 
    // Add initialization code here
-
+   storedPairs = {};
+   var o = {};
    // Add local functions here
+   function varError(x){
+      return (x === undefined || x === null);
+   };
 
+   o = {
+
+      has : function has(key){
+         if(varError(key)){
+            throw new Error('at makeMap.has; Invalid argument!'); 
+         }
+         return storedPairs.hasOwnProperty(key);
+      },
+
+
+      lookup : function lookup(key){
+         if(varError(key)){
+            throw new Error('at makeMap.lookup; Invalid argument!'); 
+         }
+         if (storedPairs.hasOwnProperty(key)) {
+             return storedPairs[key];
+         } else {
+            throw new Error('at makeMap.lookup; Key (' + key + ') doesn\'t exists!');
+         }
+      },
+
+
+      add : function add(key, value){
+         if(varError(key)||varError(value)){
+            throw new Error('at makeMap.add; Invalid argument(s)!'); 
+         }
+         if (storedPairs.hasOwnProperty(key)) {
+             throw new Error('at makeMap.add; Key (' + key + ') already exists!');
+         } else {
+            storedPairs[key] = value;
+         }
+      },
+
+      update : function update(key,value){
+         if(varError(key)||varError(value)){
+            throw new Error('at makeMap.update; Invalid argument(s)!'); 
+         }
+         if (!storedPairs.hasOwnProperty(key)) {
+             throw new Error('at makeMap.update; Key (' + key + ') doesn\'t exist!');
+         } else {
+            storedPairs[key] = value;
+            return storedPairs;
+         }
+      },
+
+      remove : function remove(key){
+         if(varError(key)){
+            throw new Error('at makeMap.delete; Invalid argument!'); 
+         }
+         if (!storedPairs.hasOwnProperty(key)) {
+             throw new Error('at makeMap.delete; Key (' + key + ') doesn\'t exist!');
+         } else {
+            delete storedPairs[key];
+         }
+      }
+   };
    // Prepare the object o before returning it
-
    return o;
 }
 
