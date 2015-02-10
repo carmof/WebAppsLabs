3be5072bf64a57603ccb14b4173d25fbef100db1
Filipe Cunha Oliveira <oliveiraf@hanover.edu>
2015-02-10 01:03:38 -0500

    Finished

--- controller.js
+++ controller.js
@@ -10,13 +10,13 @@
  */
 var makeController = function(element) {
    "use strict";
-
+   var el, tasks;
    // el is a jQuery object wrapping "element", in case it wasn't a jQuery
    // object already.
-   var el = $(element);
+   el = $(element);
 
    // "tasks" is an array of strings for the tasks to be shown.
-   var tasks = [];
+   tasks = [];
 
    // add an "add" button right after the element
    addAddButton();
@@ -33,35 +33,38 @@ var makeController = function(element) {
     *
     * These functions are meant to be used from the handlers
     */
-
    /*
     * Given a string "str", should return the html string that has a
     * list item, and in it a "span" containing the string, followed by
     * an "input" button for "remove". See examples in sample.html
     */
    function newTaskHTML(str) {
-   		var span = $("span").append("str");
-   		var input =
+      var span, input, li;
+      span = $("<span>").text(str);
+      input = $("<input>").attr("type", "button").attr("value", "Remove");
+      input.click(removeElement);
+      li = $("<li>").append(span);
+      li.append(input);
+      return li;
    }
-
    /*
     * Given an event that occured in an element within a "list element"
     * returns that "li" element (in jQuery wrapper).
     * Use jQuery's "closest".
     */
    function getLi(ev) {
-
+      var li = $(ev.target).closest("li");
+      $(li).html();
+      return li;
    }
-
    /*
     * Given a list item (as jQuery wrapper) returns its index among its
     * "li"-siblings.
     * Use jQuery's "prevAll".
     */
    function getIndex(li) {
-
+      return $(li).prevAll().length;
    }
-
    /*
     * Given a jQuery wrapper for a list item, enables "Edit mode" for that
     * item, namely:
@@ -71,9 +74,13 @@ var makeController = function(element) {
     * - Returns a reference to the jQuery wrapper of that edit element.
     */
    function enableEditMode(li) {
-
+      var input, liAux;
+      $(li).children().addClass("hidden");
+      input = $("<input>").attr("type", "text").attr("class", "edit");
+      liAux = $(li);
+      liAux.append(input);
+      return  liAux;
    }
-
    /*
     * Given a jQuery wrapper for a list item, disables "Edit mode" for that
     * item, namely:
@@ -83,15 +90,15 @@ var makeController = function(element) {
     * - Returns the list item
     */
    function disableEditMode(li) {
-
+      $(li).children(".edit").remove();
+      $(li).children().removeClass("hidden");
+      return $(li);
    }
-
    /*
     * EVENT HANDLERS
     *
     * All functions below are handling events
     */
-
    /*
     * Adds an input tag as a sibling immediately after the element.
     * Input tag should be a button, saying "New" on it.
@@ -99,18 +106,17 @@ var makeController = function(element) {
     * function.
     */
    function addAddButton() {
-      var button;   // Should be a reference to the newly created button
+     var button;
+     // Should be a reference to the newly created button
+	 button = $("<input>").attr("type", "button").attr("value", "New");
+     button.insertAfter(el);
       // Use jQuery syntax to create a new html element
       // Use appropriate append-type jQuery method to add it right after
       // "el"
-
-
       // Bind clicking of the button to calling the addNewTask function.
-
-
+      button.click(addNewTask);
       return this;
    }
-
    /*
     * Adds a new "Task". In this method you should:
     * - Push a new string with the text "New Task" at the end of the tasks array.
@@ -119,9 +125,11 @@ var makeController = function(element) {
     * - Return true to not prevent propagation.
     */
    function addNewTask(ev) {
+      tasks.push("New Task");
+      el.append(newTaskHTML(tasks[ 0 ]));
+      // ask him about the reason of returning true
       return true;
-   };
-
+   }
    /*
     * This method triggers in response to clicking the button with class
     * remove. It should:
@@ -132,10 +140,11 @@ var makeController = function(element) {
     * - Return true to not prevent propagation.
     */
    function removeElement(ev) {
+      var li = getLi(ev);
+      tasks.splice(getIndex(li), 1);
+      li.remove();
       return true;
-   };
-
-
+   }
    /*
     * This method is meant to allow editing of an item. It does this by
     * inserting an input element and hiding the existing elements.
@@ -146,9 +155,10 @@ var makeController = function(element) {
     * - Return true to allow propagation.
     */
    function editElement(ev) {
+      var li = enableEditMode(ev.target);
+      li.children("input").focus();
       return true;
-   };
-
+   }
    /*
     * This method happens when the text input where the user was editing a
     * text is registering a change, typically associated with tabbing out
@@ -173,9 +183,18 @@ var makeController = function(element) {
     * - Return true to allow propagation
     */
    function commitEditing(ev) {
+      var li, content;
+      if (ev.target === null || ev.target === "undefined"){
+         return true;
+      }
+      li = getLi(ev);
+      content = ev.target.value;
+      tasks[ getIndex(li) ] = content;
+      console.log(content);
+      li.children("span").html(content);
+      disableEditMode(li);
       return true;
-   };
-
+   }
    /*
     * This method is meant to react to the case where the user has pressed
     * the "esc" button to aknowledge that they did not mean to edit the
@@ -188,13 +207,15 @@ var makeController = function(element) {
     * - Return "false" to prevent propagation in the case of an escape.
     */
    function checkForCancel(ev) {
-      if (ev.keyCode !== 0x1B) { return true; }
-
+      var li;
+      if (ev.keyCode !== 0x1B) {
+         return true;
+      }
+      li = getLi(ev);
+      disableEditMode(li);
       return false;
-   };
-
+   }
    /* You do not need to change anything below this line */
-
    /*
     * Binds UI events to specific object methods.
     * You do not need to change this method.
@@ -205,21 +226,17 @@ var makeController = function(element) {
       el.on("click", ".remove", removeElement);
       el.on("keydown", ".edit", checkForCancel);
    }
-
    return {
       el: element,
       tasks: tasks
    };
 };
-
-
 // jQuery with function. This function runs when the page has finished
 // loading. It gets everything started
 //
 // Normally this would have been on a different file.
 $(function() {
    "use strict";
-
    // Create the specific controller object initialized to work with our page
    makeController($("#main ul"));
 });
